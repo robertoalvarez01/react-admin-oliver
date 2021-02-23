d76b3aae1e3f24a11b160b3f1d579903de63a3fc
Franco Benitez <fhbenitez@hcdn.gob.ar>
2021-02-23 14:29:20 -0300

    update

--- src/components/EnvioList.js
+++ src/components/EnvioList.js
@@ -1,29 +1,34 @@
-import React,{Fragment} from 'react';
+import React from 'react';
 import './style/EnvioList.css';
 
 class EnvioListItem extends React.Component {
   render() {
     return (
       <>
-        <tr>
+        <tr className={(this.props.envio.venta.pagado)?'nopago':'pago'}>
           {/* <th style={{verticalAlign:'baseline'}}>
             <span className="span-pagado" style={{display:'block',width:'10px',height:'10px',backgroundColor:'#439443',borderRadius:'5px'}}></span>
           </th> */}
           <th>{this.props.envio.zona || '-'}</th>
           <td>{this.props.envio.tipo}</td>
           <td className="buttonsDesktop">
-            <button className="btn btn-outline-danger mx-1" onClick={()=>this.props.delete(this.props.envio.idEnvio)}>
+            <button className="btn btn-danger mx-1" onClick={()=>this.props.delete(this.props.envio.idEnvio)}>
               <i className="fas fa-trash-alt"></i>
             </button>
-            <button className="btn btn-outline-warning mx-1" onClick={()=>this.props.mostrarDetalle(this.props.envio.venta)}>
+            <button className="btn btn-warning mx-1" onClick={()=>this.props.mostrarDetalle(this.props.envio.venta)}>
               <i className="fas fa-eye"></i>
             </button>
-            <button className={`btn btn-${(this.props.envio.entregado===0)?`outline-info`:`success`} mx-1`} disabled={(this.props.envio.en_camino===0 || this.props.envio.entregado===1)?true:false} onClick={()=>this.props.cambiarEstadoEntregado(this.props.envio.idEnvio)}>
-              {(this.props.envio.entregado===0)?`E`:`R`}
-            </button>
-            <button className={`btn btn-${(this.props.envio.en_camino===0)?`outline-info`:`danger`} mx-1`} disabled={(this.props.envio.en_camino===0)?false:true} onClick={()=>this.props.cambiarEstadoEnCamino(this.props.envio.idEnvio)}>
-              <i className="fas fa-ambulance"></i>
-            </button>
+            {(this.props.envio.idZona)?
+            <>
+              <button className={`btn btn-${(this.props.envio.entregado===0)?`info`:`success`} mx-1`} disabled={(this.props.envio.en_camino===0 || this.props.envio.entregado===1)?true:false} onClick={()=>this.props.cambiarEstadoEntregado(this.props.envio.idEnvio)}>
+                {(this.props.envio.entregado===0)?`E`:`R`}
+              </button>
+              <button className={`btn btn-${(this.props.envio.en_camino===0)?`info`:`danger`} mx-1`} disabled={(this.props.envio.en_camino===0)?false:true} onClick={()=>this.props.cambiarEstadoEnCamino(this.props.envio.idEnvio)}>
+                <i className="fas fa-ambulance"></i>
+              </button>
+            </>:
+              <button className="btn btn-success mx-1">Entregar</button>
+            }
           </td>
         </tr>
       </>
@@ -35,7 +40,7 @@ class EnvioList extends React.Component {
   render() {
     return (
         <div className="container mt-3">
-            <table className="table text-center table-hover table-envios">
+            <table className="table text-center table-envios">
                 <thead className="thead-dark">
                     <tr>
                       <th scope="col">Zona</th>
